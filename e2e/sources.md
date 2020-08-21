3f5871b3552f53a36d81c87a30345a82fe1fd8c4
Dan <williamsdaniel888@gmail.com>
2020-08-21 14:37:21 +0100

    Update copyright notices

---
+++ e2e/sources.md
@@ -0,0 +1,5 @@
+The datasets used for E2E-ABSA have been collated and formatted as JSON files from the following sources:
+- **lapt** and **rest**: The laptop and restaurant domain sets from the [SemEval-2014 Challenge](https://www.aclweb.org/anthology/S14-2004) Task 4;
+- **unified**: The restaurant domain set from [A unified model for opinion target extraction and target sentiment prediction](https://www.aaai.org/ojs/index.php/AAAI/article/view/4643);
+- **mams**: The dataset from [A Challenge Dataset and Effective
+Models for Aspect-Based Sentiment Analysis](https://www.aclweb.org/anthology/D19-1654/).
--- src/albat_asc.py
+++ src/albat_asc.py
@@ -1,11 +1,25 @@
+# Copyright 2020 Daniel Williams.
+# Contains code contributions by the Google AI Language Team, HuggingFace Inc.,
+# NVIDIA CORPORATION, authors from the University of Illinois at Chicago, and 
+# authors from the University of Parma and Adidas AG.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#    http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-# from pytorch_pretrained_albert.modeling import AlbertPreTrainedModel, AlbertModel
 from transformers.modeling_albert import AlbertPreTrainedModel, AlbertModel
 import torch
 from torch.autograd import grad
 import math
 
-# class AlbertForABSA(AlbertModel):
 class AlbertForABSA(AlbertModel):
     def __init__(self, config, num_labels=3, dropout=None, epsilon=None, od=1,use_relu=0):
         super(AlbertForABSA, self).__init__(config)
