f37de20dc696f4d32e771292444e6163d7515cdd
Dan <williamsdaniel888@gmail.com>
2020-08-21 18:45:39 +0100

    Fix albert_model argument

--- script/run_absa_traineval.sh
+++ script/run_absa_traineval.sh
@@ -43,14 +43,14 @@ do
     echo "Run $run"
     if ! [ -e $OUTPUT_DIR/"valid.json" ] ; then
        python ../src/run_$task.py \
-           --bert_model $bert --do_train --do_valid \
+           --albert_model $bert --do_train --do_valid \
            --max_seq_length 100 --train_batch_size 16 --learning_rate 3e-5 --num_train_epochs $epochs \
            --output_dir $OUTPUT_DIR --data_dir $DATA_DIR --seed $run > $OUTPUT_DIR/train_log.txt 2>&1
     fi
 
     if ! [ -e $OUTPUT_DIR/"predictions.json" ] ; then 
         python ../src/run_$task.py \
-            --bert_model $bert --do_eval --max_seq_length 100 \
+            --albert_model $bert --do_eval --max_seq_length 100 \
             --output_dir $OUTPUT_DIR --data_dir $DATA_DIR --seed $run > $OUTPUT_DIR/test_log.txt 2>&1
     fi
     #if [ -e $OUTPUT_DIR/"predictions.json" ] && [ -e $OUTPUT_DIR/model.pt ] ; then
